3a879759759b965ec620c58954fec4afbd7b5f88
Vincent Wong <vincent.tsn@gmail.com>
2014-09-17 20:03:59 +0800

    add new projects

---
+++ RideNow3/RideNow3/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using RideNow3.Resources;
+
+namespace RideNow3
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
